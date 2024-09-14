b6e8c0fe4b4cb2bc2bb903071524438098d1fa0f
Rsenal <rushik.sriramoju45@gmail.com>
2024-09-14 22:27:59 +1000

    reduced the size ofapp.js

--- backend/app.js
+++ backend/app.js
@@ -13,6 +13,7 @@ const app = express();
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
+var projectRouter = require('./routes/project');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,96 +28,7 @@ app.use(cors());
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
-
-// Sends a request to users route
-app.get('/users', (req, res) => {
-    res.send("This is the users page! Hi! this is another test");
-})
-
-// Sending and Storing data
-app.post('/user', async (req, res) => {
-    try {
-        res.send(req.body);
-        console.log(req.body);
-        const user = await User.create(req.body);
-        res.status(200).json(user);
-    } catch(error) {
-        console.log(error);
-    }
-})
-
-app.get('/signup', function(req, res) {
-    try {
-        res.send(req.body);
-        res.sendFile(__dirname + "/form.html");
-    } catch(error) {
-        console.log(error);
-    }
-})
-
-app.post('/signup', function(req, res) {
-    try {
-        // let newUser = new User({
-        //     username: await req.body.username,
-        //     password: await req.body.password,
-        //     projects: "none"
-        // });
-
-        console.log(req.body);
-        
-        var name = req.body.username; 
-        var pass = req.body.password; 
-        var projects = "none";
-      
-        var data = { 
-            username: name,
-            password: pass, 
-            projects: projects 
-        } 
-        console.log("Inserted New User");
-        const user = User.create(data);
-        res.status(200).json(user);
-        // await newUser.save();
-        // res.redirect("\form.html");
-    } catch(error) {
-        console.log(error);
-    }
-})
-
-app.get('/user', async (req, res) => {
-    try {
-        const users = await User.find({});
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
-
-// this is to get users by the id
-app.get('/user/:id', async(res, req) => {
-    try {
-        const {id} = req.params;
-        const user = await Product.findById(id);
-        res.status(200).json(product);
-    } catch(error) {
-        res.status(500).json({message: error.message});
-    }
-})
-
-// app.listen(3000, ()=>{
-//     console.log("Running on Port 3000!");
-// })
-// updating or editing data in database
-// app.put('products:id', async(req, res) => {
-//     try {
-//         const {id} = req.params;
-//         const user = await User.findByIdAndUpdate(id);
-//         // if 
-//         // if ()
-//     } catch(error) {
-//         res.status(500).json({message: error.message});
-//     }
-// })
+app.use('/project', projectRouter);
 
 const con = mongoose.connect(url)
 .then(() => {
